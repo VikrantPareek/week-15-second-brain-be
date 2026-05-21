a0fcd6a4d9385e72b28ed524bdae3f1335d59fc4
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-05-21 10:26:18 +0530

    Added fetch content route

--- src/index.ts
+++ src/index.ts
@@ -6,54 +6,75 @@ import { ContentModal, UserModal } from "./db.js";
 import jwt from "jsonwebtoken";
 import { userAuth } from "./middleware.js";
 
-let JWT_SECRET = process.env.JWT_SECRET
+let JWT_SECRET = process.env.JWT_SECRET;
 
-let app = express()
-app.use(express.json())
+let app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", function(req:Request, res:Response){
-    // password hashing
-    // zod validation
-    // password validations
-    // error handling
-    let {username, password} = req.body;
-    UserModal.create({
-        username, password
-    })
+app.post("/api/v1/signup", function (req: Request, res: Response) {
+  // password hashing
+  // zod validation
+  // password validations
+  // error handling
+  let { username, password } = req.body;
+  UserModal.create({
+    username,
+    password,
+  });
+  res.json({
+    message: "Done!",
+  });
+});
+
+app.post("/api/v1/signin", async function (req: Request, res: Response) {
+  // zod validations
+  // verify user
+  // error handling
+  let { username, password } = req.body;
+  let response = await UserModal.findOne({
+    username,
+    password,
+  });
+  if (response) {
+    let token = jwt.sign({ username: username }, JWT_SECRET as string);
     res.json({
-        message:"Done!"
-    })
-})
+      message: token,
+    });
+  } else {
+    res.json({
+      message: "Incorrect creds!",
+    });
+  }
+});
 
-app.post("/api/v1/signin", async function(req:Request, res:Response){
-    // zod validations
-    // verify user
-    // error handling
-    let {username, password} = req.body;
-    let response = await UserModal.findOne({
-        username, password
-    })
-    if(response){
-        let token = jwt.sign({username: username}, JWT_SECRET as string)
-        res.json({
-            message: token
-        })
-    }else{
-        res.json({
-            message: "Incorrect creds!"
-        })
-    }
-})
+app.post("/api/v1/content", userAuth, function (req: Request, res: Response) {
+  // zod validation
+  // error handling
+  // tags reference
+  let userId = req.userId as string;
+  let { title, link, type, tags } = req.body;
+  ContentModal.create({
+    title,
+    link,
+    type,
+    tags,
+    userId,
+  });
+  res.json({
+    message: "Content Added",
+  });
+});
 
-app.post("/api/v1/content", userAuth, function(req:Request, res:Response){
+app.get(
+  "/api/v1/content",
+  userAuth,
+  async function (req: Request, res: Response) {
     let userId = req.userId as string;
-    let {title, link, type, tags} = req.body;
-    ContentModal.create({
-        title, link, type, tags, userId
-    })
+    let response = await ContentModal.find({ userId }).populate("userId", "username");
     res.json({
-        message: "Content Added"
-    })
-})
+      message: response,
+    });
+  },
+);
 
 app.listen(3000);
